731a4cbc23a4339d95878035de2902e185ce9f81
JhojanZ <135383652+JhojanZ@users.noreply.github.com>
2024-02-27 05:40:53 -0500

    Create ui.py

---
+++ ui/ui.py
@@ -0,0 +1,8 @@
+from tabulate import tabulate
+
+def show_data(data):
+    head = ['N° Dato', 'Ciudad de Ubicación', 'Edad', 'Tipo de Recurperación', 'Fuente de contagio', 'Estado', 'País viaje']
+    
+    print("\n\n", tabulate(data, headers=head, tablefmt='mixed_grid'), sep="")
+
+# heavy_grid, fancy_grid, mixed_grid
